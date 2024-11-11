e343f82d0dc9a3509291d255ceb7bf6c309cb1ce
ale-russ <agarrussom@gmail.com>
2024-11-11 18:39:25 +0300

    changin api url

--- lms/src/api/axiosInstance.js
+++ lms/src/api/axiosInstance.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "https://lms0.onrender.com"
-
-",
+  baseURL: "https://lms-4fzy.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
